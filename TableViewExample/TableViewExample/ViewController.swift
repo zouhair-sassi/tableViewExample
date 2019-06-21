d257c77134fc4c6dcfda06cedffcf57414333af8
zouhair sassi <zouhair.sassi@nostatikmedia.com>
2019-06-21 10:16:27 +0100

    initial commit

---
+++ TableViewExample/TableViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by Zouhair Sassi on 6/21/19.
+//  Copyright © 2019 Zouhair Sassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
